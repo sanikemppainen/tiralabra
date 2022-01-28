3de26ee41cf8625e783ce58a02254792651aeb47
sanikemppainen <sani.kemppainen@helsinki.fi>
2022-01-28 13:54:24 +0200

    leaflet map

---
+++ koodi/app/src/main/java/koodi/App.java
@@ -0,0 +1,29 @@
+/*
+ * Open html file in browser
+ */
+package koodi;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+
+    public static void main(String[] args) {
+        Path path= Paths.get("app/src/index.html");
+        String htmlPath = path.toString();
+        openInBrowser(htmlPath);
+    }
+
+    public static void openInBrowser(String htmlPath){
+        try {
+            File htmlFile= new File(htmlPath);
+            Desktop.getDesktop().browse(htmlFile.toURI());
+
+        } catch (Exception e){
+            System.out.println("couldn't open html file in browser: "+ e);
+        }
+    }
+    //class joka ottaa /fromto ja resettaa etuciun eli vaan päivittää sinne kartan ja pisteet yms datan aölgoritmeista
+}
